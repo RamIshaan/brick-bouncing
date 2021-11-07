522a5424a0bd356f12749a6226275b72c955ba7c
Ram Mohan <mohan.iitb@gmail.com>
2021-11-07 13:18:39 +0530

    moving ball

--- js/game.js
+++ js/game.js
@@ -4,22 +4,41 @@ class Game {
     constructor(width, height) {
         this.boardWidth = width;
         this.boardHeight = height;
+        
         this.brickHeight = 30;
         this.brickWidth = 50;
         this.brickColor = "orange";
         this.brickGap = 5;
-        this.numRows = 4;
+        this.numRows = 6;
+        
         this.barWidth = 2*this.brickWidth;
         this.barHeight = 10;
+        this.barShiftBy = 10;
         this.barColor = "white";
+
         this.ballColor = "red";
         this.ballRadius = 12;
+
+        // game elements
         this.svgElement = document.getElementsByTagName("svg")[0];
+        this.bar = null;
+        this.ball = null;
+
+        this.gameInProgress = false;
+        this.gameProgressInterval = 30;
 
         this.buildGameUI();
+        // TODO: undertsand JS closures better
+        document.onkeydown = this.handleKeyPressed(this);
     }
 
-    start() {}
+    start() {
+        if (!this.gameInProgress) {
+            this.gameInProgress = true;
+            setInterval(this.moveBall.bind(this), this.gameProgressInterval);
+        }
+    }
+
     pause() {}
 
     buildGameUI(){
@@ -68,6 +87,7 @@ class Game {
         rect.setAttribute("width", this.barWidth);
         rect.setAttribute("height", this.barHeight);
         rect.setAttribute("fill", this.barColor);
+        this.bar = rect;
         this.svgElement.appendChild(rect);
     }
 
@@ -80,85 +100,59 @@ class Game {
         circle.setAttribute("cy", cy);
         circle.setAttribute("r", this.ballRadius);
         circle.setAttribute("fill", this.ballColor);
+        this.ball = circle;
         this.svgElement.appendChild(circle);
     }
 
+    handleKeyPressed(self){
+        return function eventHandler(event) {
+            if(event.keyCode == 37){
+                self.moveBarLeft();
+            }
+            if(event.keyCode == 39){
+                self.moveBarRight();
+            }
+            if(event.keyCode == 32){
+                self.start();
+            }
+        }
+    }
+
+    moveBarLeft(){
+        var x = parseInt(this.bar.getAttribute("x"));
+        var newXPosition = Math.max(0, x-this.barShiftBy);
+        this.bar.setAttribute("x", newXPosition);
+    }
+
+    moveBarRight(){
+        var x = parseInt(this.bar.getAttribute("x"));
+        var xEnd = x + this.barWidth;
+        var newXEnd = Math.min(this.boardWidth, xEnd + this.barShiftBy);
+        var newX = newXEnd - this.barWidth;
+        this.bar.setAttribute("x", newX);
+    
+    }
+
+    moveBall(){
+        
+        var oldCy = parseInt(this.ball.getAttribute("cy"));
+        var newCy = oldCy - this.ballRadius;
+        if (newCy <= 0) {
+            this.ball.setAttribute("cy", this.boardHeight);
+        } 
+        else {
+            this.ball.setAttribute("cy", newCy);
+        }
+    }
+
 }
-
-
-
-
-
-function buildGameUI(width, height, numRows) {
-    buildWall(width, height, numRows);
-    buildBar(145, 180, 60, 8);
-    buildBall(175, 170, 10);
-    document.onkeydown = _getKeyPressed;
-}
-
-
-
-
 
 
 
 
 
 // helper functions
-function _getSVGBox(){
-    return document.getElementsByTagName("svg")[0];
-}
-
-function _getBar(){
-    return document.getElementById("bar");
-}
-
-function _getBall(){
-    return document.getElementById("ball");
-}
 
 
 
-function _getKeyPressed(event){
-    if(event.keyCode == 37){
-        _moveBarLeft();
-    }
-    if(event.keyCode == 39){
-        _moveBarRight();
-    }
-    if(event.keyCode == 32){
-        setInterval(_moveBall, 50);
-    }
 
-}
-
-function _moveBarLeft(){
-    var bar = _getBar();
-    var x = parseInt(bar.getAttribute("x"));
-    var newXPosition = Math.max(0, x-10);
-    bar.setAttribute("x", newXPosition);
-}
-
-function _moveBarRight(){
-    var bar = _getBar();
-    var x = parseInt(bar.getAttribute("x"));
-    var width = parseInt(bar.getAttribute("width"));
-    var xEnd = x+width;
-    var newXEnd = Math.min(325, xEnd+10);
-    var newX = newXEnd-width;
-    bar.setAttribute("x", newX);
-
-}
-
-function _moveBall(){
-    var ball = _getBall();
-    var oldCy = parseInt(ball.getAttribute("cy"));
-    var r = parseInt(ball.getAttribute("r"));
-    var newCy = oldCy-r;
-    if (newCy <= 0) {
-        ball.setAttribute("cy", 200);
-    } 
-    else {
-        ball.setAttribute("cy", newCy);
-    }
-}
